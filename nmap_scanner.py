b522179d0c15d5464a7f072b93b2a814be4deee7
Gareth Jones <garethjones@cybertron.local>
2018-07-09 18:39:33 +0100

    added check to see if this host is up using .state on nmap.PortScanner()

--- nmap_scanner.py
+++ nmap_scanner.py
@@ -4,8 +4,14 @@ ns = nmap.PortScanner()
 
 print(ns.nmap_version)
 
-ns.scan('192.168.0.1','8000-8002', '-v')
+ns.scan('192.168.0.1','22-445', '-v --version-all')
 
-print(ns.scaninfo())
+#print(ns.scaninfo())
 
-print(ns.csv())
+#print(ns.csv())
+
+print(ns.scanstats())
+
+#print(ns.all_hosts())
+
+print(ns['192.168.0.1'].state()) # checks to see if this host is up
